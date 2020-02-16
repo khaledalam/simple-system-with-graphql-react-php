a1d7ad54d05813c1c4127064e1615d953b6a1e9b
Khaled Alam <khaledalam.net@gmail.com>
2020-02-16 20:42:14 +0200

    req: Encore, GraphQL, React

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,8 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
 ];
